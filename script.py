43b4093eaf4d4628b1007802822473d1967433ba
spate113 <spate113@asu.edu>
2021-12-03 23:56:38 -0700

    Extracted named entities from Tweets, creating triples and storing it in a CSV file

--- script.py
+++ script.py
@@ -8,19 +8,29 @@ from pathlib import Path
 from spacy.tokens import DocBin, Doc
 from spacy.training.example import Example
 
+import json
+
+tweets = []
+
+for line in open("sample_tweets(1).txt"):
+    temp = line.split('.')
+    tweets += temp
+
+print(tweets)
+
 nlp = spacy.load("C:\\Users\\spate113\\Desktop\\Pycharm Projects\\NERAnnotation\\output\\model-last")
 
-text = ['''India won 2 times in Australia, won odi Series in Australia, South Africa, New zealand. Also won T20 
-series in all SENA Countries.''' ]
+for doc in nlp.pipe(tweets, disable=["tagger"]):
+    if doc.ents == ():
+        continue
+    print("Text:" + doc.text)
+    print(f"spans: {[(e.text, e.label_) for e in doc.ents]}")
 
-for doc in nlp.pipe(text, disable=["tagger"]):
-    print(f"spans: {[(e.start, e.text, e.label_) for e in doc.ents]}")
-
-nlp2 = spacy.load("rel_component/training_old/training/model-best")
-
-for name, proc in nlp2.pipeline:
-    doc = proc(doc)
-
+# nlp2 = spacy.load("rel_component/training_gpu/model-best")
+# #
+# for name, proc in nlp2.pipeline:
+#     doc = proc(doc)
+#
 # print(doc._.rel.items())
 
 # for value, rel_dict in doc._.rel.items():
@@ -32,9 +42,9 @@ for name, proc in nlp2.pipeline:
 #                         print(f" entities: {e.text, b.text} --> predicted relation: {rel_dict}")
 
 
-for value, rel_dict in doc._.rel.items():
-        for e in doc.ents:
-            for b in doc.ents:
-                if e.start == value[0] and b.start == value[1]:
-                    if rel_dict['WONAGAINST'] >= 1.00e-10:
-                        print(f" entities: {e.text, b.text} --> predicted relation: {rel_dict}")
+# for value, rel_dict in doc._.rel.items():
+#     for e in doc.ents:
+#         for b in doc.ents:
+#             if e.start == value[0] and b.start == value[1]:
+#                 if rel_dict['WONAGAINST'] >= 1.00e-10:
+#                     print(f" entities: {e.text, b.text} --> predicted relation: {rel_dict}")
